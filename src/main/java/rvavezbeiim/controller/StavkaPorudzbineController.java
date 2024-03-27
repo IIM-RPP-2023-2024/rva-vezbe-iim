c4945070bb1869db57dcad8a8f5a9bf7f4ecccbc
staka99 <marko.staka12@gmail.com>
2024-03-27 12:24:36 +0100

    Vezbe IV - Kompletirani kontroleri

--- src/main/java/rvavezbeiim/controller/StavkaPorudzbineController.java
+++ src/main/java/rvavezbeiim/controller/StavkaPorudzbineController.java
@@ -1,13 +1,18 @@
 package rvavezbeiim.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rvavezbeiim.model.Porudzbina;
@@ -24,6 +29,22 @@ public class StavkaPorudzbineController {
 	@Autowired
 	private PorudzbinaService porudzbinaService;
 	
+    @GetMapping("stavkaPorudzbine")
+    public ResponseEntity<List<StavkaPorudzbine>> getAll() {
+        List<StavkaPorudzbine> stavkaPorudzbines = stavkaPorudzbineService.getAll();
+        return new ResponseEntity<>(stavkaPorudzbines, HttpStatus.OK);
+    }
+    
+    @GetMapping("stavkaPorudzbine/{id}")
+    public ResponseEntity<StavkaPorudzbine> getOne(@PathVariable("id") Integer id) {
+        if (stavkaPorudzbineService.findById(id).isPresent()) {
+            Optional<StavkaPorudzbine> stavkaPorudzbineOpt = stavkaPorudzbineService.findById(id);
+            return new ResponseEntity<>(stavkaPorudzbineOpt.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+	
 	@GetMapping("/stavkaPorudzbine/Cena/{cena}")
 	public ResponseEntity<List<StavkaPorudzbine>> getStavkaPorudzbineCena(@PathVariable("cena") double cena) {
 		List<StavkaPorudzbine> stavkaPorudzbines = stavkaPorudzbineService.findByCenaLessThanOrderById(cena);
@@ -39,5 +60,38 @@ public class StavkaPorudzbineController {
 		}
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
+	
+    @PostMapping("stavkaPorudzbine")
+    public ResponseEntity<StavkaPorudzbine> addOne(@RequestBody StavkaPorudzbine stavkaPorudzbine) {
+        if (!porudzbinaService.existsById(stavkaPorudzbine.getPorudzbina().getId())) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        StavkaPorudzbine savedStavkaPorudzbine = stavkaPorudzbineService.save(stavkaPorudzbine);
+        URI location = URI.create("/stavkaPorudzbine/" + savedStavkaPorudzbine.getId());
+        return ResponseEntity.created(location).body(savedStavkaPorudzbine);
+    }
+
+   @PutMapping("stavkaPorudzbine/{id}")
+    public ResponseEntity<StavkaPorudzbine> updateOne(@RequestBody StavkaPorudzbine stavkaPorudzbine,
+            @PathVariable("id") Integer id) {
+        if (!stavkaPorudzbineService.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        stavkaPorudzbine.setId(id);
+        StavkaPorudzbine savedStavkaPorudzbine = stavkaPorudzbineService.save(stavkaPorudzbine);
+        return ResponseEntity.ok().body(savedStavkaPorudzbine);
+    }
+
+   @DeleteMapping("stavkaPorudzbine/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable Integer id) {
+
+
+        if (stavkaPorudzbineService.existsById(id)) {
+            stavkaPorudzbineService.deleteById(id);
+            return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
+    }
 
 }
